ac6572f0cc52bdd0fcbb9d3e0e5545e884211d1d
David Henrique <dh210605@gmail.com>
2024-10-02 23:34:32 -0300

    Pondo Lib do AntDesign e pondo NavBar

--- src/App.js
+++ src/App.js
@@ -1,31 +1,71 @@
-import React from 'react';
-import {BrowserRouter as Router,Route,Routes,Link} from 'react-router-dom';
-import HomeComponent from './HomeComponent';
-import SobreComponent from './SobreComponent';
+import React, { useState } from 'react';
+import {
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+  HomeOutlined,
+  InfoCircleOutlined,
+} from '@ant-design/icons';
+import { Button, Menu } from 'antd';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import HomeComponent from './components/HomeComponent';
+import SobreComponent from './components/SobreComponent';
 
+const App = () => {
+  const [collapsed, setCollapsed] = useState(false);
+  
+  const toggleCollapsed = () => {
+    setCollapsed(!collapsed);
+  };
 
-function App() {
+  const items = [
+    {
+      key: 'home',
+      icon: <HomeOutlined />,
+      label: <Link to="/home">Home</Link>,
+    },
+    {
+      key: 'sobre',
+      icon: <InfoCircleOutlined />,
+      label: <Link to="/sobre">Sobre</Link>,
+    },
+  ];
+
   return (
     <Router>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/home"><button>Home</button></Link>
-          </li>
-          <li>
-            <Link to="/sobre"><button>Sobre</button></Link>
-          </li>
-        </ul>
-      </nav>
+      <div style={{ display: 'flex', height: '100vh' }}>
+        <div
+          style={{
+            width: 256,
+            backgroundColor: '#001529',
+          }}
+        >
+          <Button
+            type="primary"
+            onClick={toggleCollapsed}
+            style={{
+              margin: 16,
+            }}
+          >
+            {collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+          </Button>
+          <Menu
+            defaultSelectedKeys={['home']}
+            mode="inline"
+            theme="dark"
+            inlineCollapsed={collapsed}
+            items={items}
+          />
+        </div>
 
-      <Routes>
-        <Route path="/home" element={<HomeComponent />} />
-        <Route path="/sobre" element={<SobreComponent />} />
-      </Routes>
+        <div style={{ flex: 1, padding: '20px' }}>
+          <Routes>
+            <Route path="/home" element={<HomeComponent />} />
+            <Route path="/sobre" element={<SobreComponent />} />
+          </Routes>
+        </div>
+      </div>
     </Router>
   );
 };
-  
-
 
 export default App;
