c622eeb1478332289ac402bec4471c8c8fc35f96
David Pontes <pondavid103@gmail.com>
2024-10-06 22:54:07 -0300

    add: componentes da tela Home

--- src/components/inity/HomeComponent.js
+++ src/components/inity/HomeComponent.js
@@ -2,14 +2,41 @@ import React from 'react';
 import './Home.css';
 
 const HomeComponent = () => {
-return (
-    <div style={{textAlign:'center'}}>
-        <h1>Bem vindo</h1>
-        <p>Esta é a pagina inicial do nosso site! Sinta-se a vontade para explorar e conhecer.</p>
-    </div>
-);
-}
+  return (
+    <div className="home-container">
+      <nav className="navbar">
+        <ul>
+          <li>Home</li>
+          <li>Explore</li>
+          <li>Provas</li>
+        </ul>
+      </nav>
 
+      <div className="main-content">
+        <div className="text-section">
+          <h1>Educa+</h1>
+          <p>
+            Lorem ipsum dolor sit amet et delectus accommodare his consul copiosae 
+            legendos at vix ad putent delectus delicata usu. Vidit dissentiet eos 
+            cu eum an brute copiosae hendrerit.
+          </p>
+        </div>
+
+        <div className="card">
+          <h2>Social Education</h2>
+          <p>Lorem ipsum dolor sit amet et delectus</p>
+          <button>Button</button>
+        </div>
+      </div>
+
+      <div className="toggle-switch">
+        <span>Sample Text</span>
+        <div className="switch">
+          <div className="toggle"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default HomeComponent;
-
