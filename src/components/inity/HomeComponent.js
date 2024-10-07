16e503959ebc85703cbcb89a6e761dfac3e755cb
Eriick1945 <162646349+Eriick1945@users.noreply.github.com>
2024-10-07 17:00:14 +0000

    add: componentes de perfis

--- src/components/inity/HomeComponent.js
+++ src/components/inity/HomeComponent.js
@@ -1,5 +1,35 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Home.css';
+
+const ProfileCard = ({ name, job, description, imgSrc }) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+  const [isSelected, setIsSelected] = useState(false);
+
+  const toggleDescription = () => {
+    setIsExpanded(!isExpanded);
+  };
+
+  const toggleSelection = () => {
+    setIsSelected(!isSelected);
+  };
+
+  return (
+    <div
+      className={`profile-card ${isSelected ? "selected" : ""}`}
+      onClick={toggleSelection}
+    >
+      <img src={imgSrc} alt={`${name} profile`} className="profile-img" />
+      <h2>{name}</h2>
+      <h4>{job}</h4>
+      <p>
+        {isExpanded ? description : `${description.substring(0, 50)}...`}{" "}
+        <button onClick={toggleDescription} className="read-more">
+          {isExpanded ? "Read Less" : "Read More"}
+        </button>
+      </p>
+    </div>
+  );
+};
 
 const HomeComponent = () => {
   return (
@@ -35,6 +65,63 @@ const HomeComponent = () => {
           <div className="toggle"></div>
         </div>
       </div>
+      <h1>Perfis</h1>
+      <div className="profile-container">
+        <ProfileCard
+          name="Jose Almeida"
+          job="ETE PORTO DIGITAL"
+          description=" Lorem ipsum dolor sit amet et delectus accommodare his consul copiosae 
+            legendos at vix ad putent delectus delicata usu. Vidit dissentiet eos 
+            cu eum an brute copiosae hendrerit."
+          imgSrc="https://static.vecteezy.com/ti/vetor-gratis/p1/11186876-simbolo-de-foto-de-perfil-masculino-vetor.jpg"
+        />
+        <ProfileCard
+          name="Jose Almeida"
+          job="ETE PORTO DIGITAL"
+          description=" Lorem ipsum dolor sit amet et delectus accommodare his consul copiosae 
+            legendos at vix ad putent delectus delicata usu. Vidit dissentiet eos 
+            cu eum an brute copiosae hendrerit."
+          imgSrc="https://static.vecteezy.com/ti/vetor-gratis/p1/11186876-simbolo-de-foto-de-perfil-masculino-vetor.jpg"
+        />
+
+        <ProfileCard
+          name="Jose Almeida"
+          job="ETE PORTO DIGITAL"
+          description=" Lorem ipsum dolor sit amet et delectus accommodare his consul copiosae 
+            legendos at vix ad putent delectus delicata usu. Vidit dissentiet eos 
+            cu eum an brute copiosae hendrerit."
+          imgSrc="https://static.vecteezy.com/ti/vetor-gratis/p1/11186876-simbolo-de-foto-de-perfil-masculino-vetor.jpg"
+        />
+
+        <ProfileCard
+          name="Jose Almeida"
+          job="ETE PORTO DIGITAL"
+          description=" Lorem ipsum dolor sit amet et delectus accommodare his consul copiosae 
+            legendos at vix ad putent delectus delicata usu. Vidit dissentiet eos 
+            cu eum an brute copiosae hendrerit."
+          imgSrc="https://static.vecteezy.com/ti/vetor-gratis/p1/11186876-simbolo-de-foto-de-perfil-masculino-vetor.jpg"
+        />
+
+        <ProfileCard
+          name="Jose Almeida"
+          job="ETE PORTO DIGITAL"
+          description=" Lorem ipsum dolor sit amet et delectus accommodare his consul copiosae 
+            legendos at vix ad putent delectus delicata usu. Vidit dissentiet eos 
+            cu eum an brute copiosae hendrerit."
+          imgSrc="https://static.vecteezy.com/ti/vetor-gratis/p1/11186876-simbolo-de-foto-de-perfil-masculino-vetor.jpg"
+        />
+
+        <ProfileCard
+          name="Jose Almeida"
+          job="ETE PORTO DIGITAL"
+          description=" Lorem ipsum dolor sit amet et delectus accommodare his consul copiosae 
+            legendos at vix ad putent delectus delicata usu. Vidit dissentiet eos 
+            cu eum an brute copiosae hendrerit."
+          imgSrc="https://static.vecteezy.com/ti/vetor-gratis/p1/11186876-simbolo-de-foto-de-perfil-masculino-vetor.jpg"
+        />
+
+      </div>
+
     </div>
   );
 };
