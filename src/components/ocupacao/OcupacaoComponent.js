e26b04e345dea9cd0c0961e70a3de4b6a7aea8cc
David Henrique <dh210605@gmail.com>
2024-10-25 21:28:37 -0300

    feat/TelaEscolhaFigma - tela de escolha de cadastro

--- src/components/ocupacao/OcupacaoComponent.js
+++ src/components/ocupacao/OcupacaoComponent.js
@@ -1,74 +1,43 @@
 import React from "react";
-import { Card, Space, Button, Avatar } from "antd";
-import { UserOutlined } from "@ant-design/icons";
 import { useNavigate } from "react-router-dom";
+import "./Ocupacao.css";
 
 const Home = () => {
-  const navigate = useNavigate(); 
+  const navigate = useNavigate();
 
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
-        height: "100vh",
-      }}
-    >
-      <h1 style={{ marginBottom: "20px", textAlign: "center", position: 'relative', top:'-80px' }}>
-        Escolha o tipo de conta abaixo
-      </h1>
+    <div className="container">
+      <h1 className="title">Escolha o tipo de conta abaixo</h1>
 
-      <Space direction="horizontal" size={32}>
-        <Card
-          headStyle={{
-            borderBottom: "2px solid black",
-            textAlign: "center",
-          }}
-          style={{
-            width: 300,
-            textAlign: "center",
-            border: "2px solid black",
-          }}
-        >
-          <div style={{ textAlign: "center", marginBottom: 16 }}>
-            <Avatar size={64} icon={<UserOutlined />} />
-            <h3>Sou Aluno</h3>
-            <hr />
-          </div>
-          <p>Estou aqui pra aprender</p>
-          <p>e poder ajudar os que precisam!</p>
-          <br />
-          <Button type="primary" onClick={() => navigate("/formaluno")}>
-            Aluno
-          </Button>
-        </Card>
+      <div className="cardContainer">
+        <div className="ant-card">
+          <img
+            src={`${process.env.PUBLIC_URL}/Aluno.png`}
+            alt="Aluno"
+            className="card-image"
+          />
+          <button
+            className="btn-aluno"
+            onClick={() => navigate("/formaluno")}
+          >
+            Estou aqui pra aprender
+          </button>
+        </div>
 
-        <Card
-          headStyle={{
-            borderBottom: "2px solid black",
-            textAlign: "center",
-          }}
-          style={{
-            width: 300,
-            textAlign: "center",
-            border: "2px solid black",
-          }}
-        >
-          <div style={{ textAlign: "center", marginBottom: 16 }}>
-            <Avatar size={64} icon={<UserOutlined />} />
-            <h3>Sou Professor</h3>
-            <hr />
-          </div>
-          <p>Estou aqui para ensinar</p>
-          <p>e ajudar os alunos a aprenderem!</p>
-          <br />
-          <Button type="primary" onClick={() => navigate("/formprofessor")}>
-            Docente
-          </Button>
-        </Card>
-      </Space>
+        <div className="ant-card">
+          <img
+            src={`${process.env.PUBLIC_URL}/PROFESSOR.png`}
+            alt="Professor"
+            className="card-image"
+          />
+          <button
+            className="btn-professor"
+            onClick={() => navigate("/formprofessor")}
+          >
+            Estou aqui para ensinar
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
