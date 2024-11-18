45287b92322bcbe24303e7fc8617f38af0ea7bd6
David Pontes <pondavid103@gmail.com>
2024-11-18 19:50:07 -0300

    adicionando novamente algumas telas

--- src/components/minhasQuestoes/MinhasQuestoesComponent.jsx
+++ src/components/minhasQuestoes/MinhasQuestoesComponent.jsx
@@ -23,43 +23,43 @@ const QuestionsComponent = () => {
     fetchQuestions();
   }, []);
 
-  if (loading) {
-    return (
-      <div className="loading-container">
-        <div className="spinner"></div>
-        <p className="loading-message">Carregando as melhores questões para você...</p>
-      </div>
-    );
-  }
-
-  if (error) {
-    return <p>{error}</p>;
-  }
-
   return (
     <div>
       <ResponsiveAppBar />
-      <div style={{ textAlign: 'center', marginBottom: '20px' }}>
-        <h1>Praticar questões</h1>
-        <p>Pesquise, favorite e faça as questões da sua escolha</p>
-      </div>
-      <div className="questions-container">
-        {questions.map((question) => (
-          <div className="question-card" key={question.id}>
-            <div className="header">{question.cabecalho}</div>
-            <div className="body">
-              <p className="enunciado">{question.enunciado}</p>
-              <ul className="alternativas">
-                {question.alternativas.map((alternativa, index) => (
-                  <li key={index} className="alternativa">
-                    {alternativa}
-                  </li>
-                ))}
-              </ul>
-            </div>
+      {loading ? (
+        <div className="loading-container">
+          <div className="spinner"></div>
+          <p className="loading-message">Carregando as melhores questões para você...</p>
+        </div>
+      ) : error ? (
+        <div style={{ textAlign: 'center', marginTop: '20px' }}>
+          <h2>{error}</h2>
+        </div>
+      ) : (
+        <div>
+          <div style={{ textAlign: 'center', marginBottom: '20px' }}>
+            <h1>Praticar questões</h1>
+            <p>Pesquise, favorite e faça as questões da sua escolha</p>
           </div>
-        ))}
-      </div>
+          <div className="questions-container">
+            {questions.map((question) => (
+              <div className="question-card" key={question.id}>
+                <div className="header">{question.cabecalho}</div>
+                <div className="body">
+                  <p className="enunciado">{question.enunciado}</p>
+                  <ul className="alternativas">
+                    {question.alternativas.map((alternativa, index) => (
+                      <li key={index} className="alternativa">
+                        {alternativa}
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
     </div>
   );
 };
---
+++ src/components/simulados/SimuladosComponent.js
@@ -0,0 +1,84 @@
+import React, { useRef, useState } from "react";
+import "./SimuladosComponent.css";
+import documentoImg from "../assets/documents.png";
+import axios from "axios";
+import ResponsiveAppBar from "../header/Header";
+
+const MinhasQuestoesComponent = () => {
+  const [mensagemSucesso, setMensagemSucesso] = useState(false);
+  const fileInputRef = useRef(null);
+  const questoes = Array(8).fill({
+    nome: "Carlos Alberto",
+    foto: "",
+  });
+
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      handleUpload(file);
+    }
+  };
+
+  const handleUpload = (file) => {
+    setMensagemSucesso(true);
+    setTimeout(() => setMensagemSucesso(false), 5000);
+
+    const formData = new FormData();
+    formData.append("file", file);
+
+    const apiUrl = "http://localhost:8080/serviceIA/processar-pdf";
+
+    axios
+      .post(apiUrl, formData)
+      .then((response) => {
+        console.log("Sucesso:", response.data);
+      })
+      .catch((error) => {
+        console.error("Erro:", error.response || error.message || error);
+      });
+  };
+
+  const triggerFileInput = () => {
+    fileInputRef.current.click();
+  };
+
+  return (
+    <div>
+      <ResponsiveAppBar />
+      <div className="containerr">
+        {mensagemSucesso && (
+          <div className="alert-mensagem">
+            Arquivo enviado com sucesso!
+          </div>
+        )}
+        <div className="scanner">
+          <img src={documentoImg} alt="Scanner Icon" className="scanner-icon" />
+          <input
+            type="file"
+            accept="application/pdf"
+            onChange={handleFileChange}
+            ref={fileInputRef}
+            style={{ display: "none" }}
+          />
+          <button className="scanner-button" onClick={triggerFileInput}>
+            Scannerar Documento
+          </button>
+        </div>
+        <div className="questoes-container">
+          {questoes.map((questao, index) => (
+            <div key={index} className="questao-card">
+              <p className="lista-de">Lista de</p>
+              <div className="perfil">
+                <img src={questao.foto} alt="Foto de perfil" className="foto" />
+                <p className="nome">{questao.nome}</p>
+              </div>
+              <button className="acessar-button">Acessar</button>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MinhasQuestoesComponent;
